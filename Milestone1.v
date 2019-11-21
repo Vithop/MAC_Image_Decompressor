0af4afd5b687fe0f882e51a0f007fc6a274a8f59
Mathuran Sadagopan <mathuransada@gmail.com>
2019-11-21 00:06:47 -0500

    added the bosses sram and fixed latching behaivior

--- Milestone1.v
+++ Milestone1.v
@@ -101,7 +101,7 @@ always_comb begin
 		Op4 = 31'd52;
 		Op5 = V_buffer[3] + V_buffer[2];
 		Op6 = 31'd159;
-		V_prime = (result_a - result_b + result_c + 32'd128) >>> 8;
+		// V_prime = (result_a - result_b + result_c + 32'd128) >>> 8;
 	end else if (M1_state == S_M1_CALC_U_PRIME || M1_state == S_M1_LI_CALC_U || M1_state == S_M1_LO_WRITE_GB) begin
 		Op1 = U_buffer[5] + U_buffer[0];
 		Op2 = 31'd21;
@@ -109,7 +109,7 @@ always_comb begin
 		Op4 = 31'd52;
 		Op5 = U_buffer[3] + U_buffer[2];
 		Op6 = 31'd159;	
-		U_prime = (result_a - result_b + result_c + 32'd128) >>> 8;
+		// U_prime = (result_a - result_b + result_c + 32'd128) >>> 8;
 	end else if (M1_state == S_M1_CALC_FIRST_RB || M1_state == S_M1_LO_CALC_FIRST_RB) begin
 		Op1 = Y_buffer[1] - 31'd16;
 		Op2 = 31'd76284;
@@ -117,16 +117,16 @@ always_comb begin
 		Op4 = 31'd132251;
 		Op5 = V_buffer[2] - 31'd128;
 		Op6 = 31'd104595;
-		R_temp = (result_a + result_c);
-		B_temp = (result_a + result_b);
+		// R_temp = (result_a + result_c);
+		// B_temp = (result_a + result_b);
 
-		R_even = (R_temp[31] == 1'd1)
-					? 8'd0
-					: |R_temp[30:24] ? 8'd255 : R_temp >>> 16;
+		// R_even = (R_temp[31] == 1'd1)
+		// 			? 8'd0
+		// 			: |R_temp[30:24] ? 8'd255 : R_temp >>> 16;
 
-		B_even = (B_temp[31] == 1'd1)
-					? 8'd0
-					: |B_temp[30:24] ? 8'd255 : B_temp >>> 16;
+		// B_even = (B_temp[31] == 1'd1)
+		// 			? 8'd0
+		// 			: |B_temp[30:24] ? 8'd255 : B_temp >>> 16;
 	end else if (M1_state == S_M1_CALC_SECOND_RB || M1_state == S_M1_LO_CALC_SECOND_RB) begin
 		Op1 = Y_buffer[0] - 31'd16;
 		Op2 = 31'd76284;
@@ -134,16 +134,16 @@ always_comb begin
 		Op4 = 31'd132251;
 		Op5 = V_prime - 31'd128;
 		Op6 = 31'd104595;
-		R_temp = (result_a + result_c);
-		B_temp = (result_a + result_b);
+		// R_temp = (result_a + result_c);
+		// B_temp = (result_a + result_b);
 
-		R_odd = (R_temp[31] == 1'd1)
-					? 8'd0
-					: |R_temp[30:24] ? 8'd255 : R_temp >>> 16;
+		// R_odd = (R_temp[31] == 1'd1)
+		// 			? 8'd0
+		// 			: |R_temp[30:24] ? 8'd255 : R_temp >>> 16;
 
-		B_odd = (B_temp[31] == 1'd1)
-					? 8'd0
-					: |B_temp[30:24] ? 8'd255 : B_temp >>> 16;
+		// B_odd = (B_temp[31] == 1'd1)
+		// 			? 8'd0
+		// 			: |B_temp[30:24] ? 8'd255 : B_temp >>> 16;
 	end else if (M1_state == S_M1_CALC_FIRST_G || M1_state == S_M1_LO_CALC_FIRST_G) begin
 		Op1 = Y_buffer[1] - 31'd16;
 		Op2 = 31'd76284;
@@ -151,10 +151,10 @@ always_comb begin
 		Op4 = 31'd25624;
 		Op5 = V_buffer[2] - 31'd128;
 		Op6 = 31'd53281;
-		G_temp  = (result_a - result_b - result_c);
-		G_even = (G_temp[31] == 1'd1)
-					? 8'd0
-					: |G_temp[30:24] ? 8'd255 : G_temp >>> 16;
+		// G_temp  = (result_a - result_b - result_c);
+		// G_even = (G_temp[31] == 1'd1)
+		// 			? 8'd0
+		// 			: |G_temp[30:24] ? 8'd255 : G_temp >>> 16;
 	end else if (M1_state == S_M1_CALC_SECOND_G || M1_state == S_M1_LO_CALC_SECOND_G) begin
 		Op1 = Y_buffer[0] - 31'd16;
 		Op2 = 31'd76284;
@@ -162,10 +162,10 @@ always_comb begin
 		Op4 = 31'd25624;
 		Op5 = V_prime - 31'd128;
 		Op6 = 31'd53281;
-		G_temp  = (result_a - result_b - result_c);
-		G_odd = (G_temp[31] == 1'd1)
-					? 8'd0
-					: |G_temp[30:24] ? 8'd255 : G_temp >>> 16;
+		// G_temp  = (result_a - result_b - result_c);
+		// G_odd = (G_temp[31] == 1'd1)
+		// 			? 8'd0
+		// 			: |G_temp[30:24] ? 8'd255 : G_temp >>> 16;
 	end else begin
 		Op1 = 31'd0;
 		Op2 = 31'd0;
@@ -247,31 +247,61 @@ always @(posedge Clock or negedge Resetn) begin
 				M1_state <= S_M1_LI_CALC_V;
 			end
 			S_M1_LI_CALC_V:begin
+				V_prime <= (result_a - result_b + result_c + 32'd128) >>> 8;
 				U_buffer <= {SRAM_read_data[7:0], SRAM_read_data[15:8], U_buffer[5:2]};
 				M1_state <= S_M1_LI_CALC_U;
 			end
 			S_M1_LI_CALC_U:begin
+				U_prime <= (result_a - result_b + result_c + 32'd128) >>> 8;
 				M1_state <= S_M1_CALC_FIRST_RB;
 			end
 			//****START OF REPEATING CYCLES
 			S_M1_CALC_FIRST_RB:begin
+				R_temp <= (result_a + result_c);
+				B_temp <= (result_a + result_b);
+				R_even = (R_temp[31] == 1'd1)
+					? 8'd0
+					: |R_temp[30:24] ? 8'd255 : R_temp >>> 16;
+
+				B_even = (B_temp[31] == 1'd1)
+					? 8'd0
+					: |B_temp[30:24] ? 8'd255 : B_temp >>> 16;
+
+				SRAM_we_n <= 1'b1;
+				M1_state <= S_M1_CALC_FIRST_G;
+
 				if(read_UV_flag == 1'b1) begin
 					SRAM_address = intit_V_address + UV_count;
 				end else begin
 					Y_count <= Y_count + 1'd1;
 					SRAM_address = intit_Y_address + Y_count;
 				end
-				SRAM_we_n <= 1'b1;
-				M1_state <= S_M1_CALC_FIRST_G;
 			end
 			S_M1_CALC_FIRST_G:begin
+				G_temp  <= (result_a - result_b - result_c);
+				G_even <= (G_temp[31] == 1'd1)
+							? 8'd0
+							: |G_temp[30:24] ? 8'd255 : G_temp >>> 16;
+
+				M1_state <= S_M1_CALC_SECOND_RB;
+
 				if(read_UV_flag == 1'b1) begin
 					UV_count <= UV_count + 1'd1;
 					SRAM_address = intit_U_address + UV_count;
 				end
-				M1_state <= S_M1_CALC_SECOND_RB;
 			end
 			S_M1_CALC_SECOND_RB:begin
+				R_temp <= (result_a + result_c);
+				B_temp <= (result_a + result_b);
+
+				R_odd <= (R_temp[31] == 1'd1)
+							? 8'd0
+							: |R_temp[30:24] ? 8'd255 : R_temp >>> 16;
+
+				B_odd <= (B_temp[31] == 1'd1)
+							? 8'd0
+							: |B_temp[30:24] ? 8'd255 : B_temp >>> 16;
+
 				M1_state <= S_M1_CALC_SECOND_G;
 				if(read_UV_flag == 1'b1) begin
 					Y_count <= Y_count + 1'd1;
@@ -285,6 +315,12 @@ always @(posedge Clock or negedge Resetn) begin
 				end
 			end
 			S_M1_CALC_SECOND_G:begin
+
+				G_temp  <= (result_a - result_b - result_c);
+				G_odd <= (G_temp[31] == 1'd1)
+							? 8'd0
+							: |G_temp[30:24] ? 8'd255 : G_temp >>> 16;
+
 				SRAM_we_n <= 1'b0;
 				SRAM_address <= init_RGB_address + RGB_count;
 				RGB_count <= RGB_count + 1'd1;
@@ -303,6 +339,7 @@ always @(posedge Clock or negedge Resetn) begin
 
 			end
 			S_M1_CALC_V_PRIME:begin
+				V_prime <= (result_a - result_b + result_c + 32'd128) >>> 8;
 				//SRAM_we_n <= 1'b1;
 				SRAM_address <= init_RGB_address + RGB_count;
 				RGB_count <= RGB_count + 1'd1;
@@ -320,6 +357,8 @@ always @(posedge Clock or negedge Resetn) begin
 				end
 			end
 			S_M1_CALC_U_PRIME:begin
+				U_prime <= (result_a - result_b + result_c + 32'd128) >>> 8;
+
 				read_UV_flag <= ~read_UV_flag;
 				
 				if ((Y_count % 160) == 157) begin
@@ -344,14 +383,38 @@ always @(posedge Clock or negedge Resetn) begin
 					SRAM_address = intit_Y_address + Y_count;
 					Y_count <= Y_count + 16'd1;
 				end
-				
+				R_temp <= (result_a + result_c);
+				B_temp <= (result_a + result_b);
+				R_even <= (R_temp[31] == 1'd1)
+					? 8'd0
+					: |R_temp[30:24] ? 8'd255 : R_temp >>> 16;
+
+				B_even <= (B_temp[31] == 1'd1)
+					? 8'd0
+					: |B_temp[30:24] ? 8'd255 : B_temp >>> 16;
 				M1_state <= S_M1_LO_CALC_FIRST_G;
 			end
 			S_M1_LO_CALC_FIRST_G:begin
+				G_temp  <= (result_a - result_b - result_c);
+				G_even <= (G_temp[31] == 1'd1)
+							? 8'd0
+							: |G_temp[30:24] ? 8'd255 : G_temp >>> 16;
 
 				M1_state <= S_M1_LO_CALC_SECOND_RB;
 			end
 			S_M1_LO_CALC_SECOND_RB:begin
+
+				R_temp <= (result_a + result_c);
+				B_temp <= (result_a + result_b);
+
+				R_odd <= (R_temp[31] == 1'd1)
+							? 8'd0
+							: |R_temp[30:24] ? 8'd255 : R_temp >>> 16;
+
+				B_odd <= (B_temp[31] == 1'd1)
+							? 8'd0
+							: |B_temp[30:24] ? 8'd255 : B_temp >>> 16;
+
 				SRAM_we_n <= 1'b0;
 				SRAM_write_data <= {R_even, G_even};
 				SRAM_address <= init_RGB_address + RGB_count;
@@ -360,17 +423,26 @@ always @(posedge Clock or negedge Resetn) begin
 				M1_state <= S_M1_LO_CALC_SECOND_G;
 			end
 			S_M1_LO_CALC_SECOND_G:begin
+
+				G_temp  <= (result_a - result_b - result_c);
+				G_odd <= (G_temp[31] == 1'd1)
+							? 8'd0
+							: |G_temp[30:24] ? 8'd255 : G_temp >>> 16;
+
 				SRAM_address <= init_RGB_address + RGB_count;
 				RGB_count <= RGB_count + 1'd1;
 				SRAM_write_data <= {B_even, R_odd};
+				M1_state <= S_M1_LO_WRITE_BR;
+
 				if ((Y_count != 31'd38401) && (LP_flag == 1'b0)) begin
 					V_buffer <= {V_odd,V_buffer[5:1]};	
 					Y_buffer <= {SRAM_read_data[15:8], SRAM_read_data[7:0]};
 				end
 				
-				M1_state <= S_M1_LO_WRITE_BR;
 			end
 			S_M1_LO_WRITE_BR:begin
+				V_prime <= (result_a - result_b + result_c + 32'd128) >>> 8;
+
 				SRAM_address <= init_RGB_address + RGB_count;
 				RGB_count <= RGB_count + 1'd1;
 				SRAM_write_data <= {G_odd, B_odd};
@@ -382,6 +454,8 @@ always @(posedge Clock or negedge Resetn) begin
 				M1_state <= S_M1_LO_WRITE_GB;
 			end
 			S_M1_LO_WRITE_GB:begin
+				U_prime <= (result_a - result_b + result_c + 32'd128) >>> 8;
+
 				if (Y_count == 31'd38401) begin
 					M1_state <= S_M1_IDLE;
 				end else if(LP_flag == 1'b1)begin
