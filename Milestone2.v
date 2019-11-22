4601ef1b4202343b58685aa1cada3cd9839f0cf0
Mathuran Sadagopan <mathuransada@gmail.com>
2019-11-22 18:23:14 -0500

    small change

--- Milestone2.v
+++ Milestone2.v
@@ -186,7 +186,7 @@ always @(posedge Clock or negedge Resetn) begin
 					M2_state <= S_M2_LI_READ_BLOCK1_1;
 				end
 			end 
-			S_M2_LI_:begin
+			S_M2_LI_READ_BLOCK1_1:begin
 				 SRAM_address <= block_index + A_i + row_address;
 				 A_i <= A_i + 3'd1;
 				 M2_state <= S_M2_LI_READ_BLOCK1_2
@@ -229,6 +229,7 @@ always @(posedge Clock or negedge Resetn) begin
 				 	end
 				 end
 			end
+			S_M2_CT_LI_
 			// A_i <= A_i + 3'd1;
 			// 	matrix_A_row[0] <= matrix_A_val;
 			// 	matrix_A_row[1] <= matrix_A_val[0];
